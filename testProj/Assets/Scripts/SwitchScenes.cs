44f7826fac3c076cd7231e3f1cfafeba5e9e476c
DJDomingo <djdomingo71@gmail.com>
2018-10-24 22:31:02 -0700

    Main Screen added
    I added a simple Main Menu Screen Sceen which we can add things to and make pretty later.

---
+++ testProj/Assets/Scripts/SwitchScenes.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SwitchScenes : MonoBehaviour {
+
+	public void LoadScene(string name) {
+		SceneManager.LoadScene (sceneName: name);
+	}
+
+	public void QuitGame() {
+		Application.Quit ();
+	}
+}
